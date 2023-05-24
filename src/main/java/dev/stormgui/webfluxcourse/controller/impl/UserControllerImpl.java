82569aeffaef0f2c5ac79a8aa73626b249560133
Guilherme Chamorro <poltegaisnt@gmail.com>
2023-05-23 21:21:57 -0300

    add mapper to user and impl to controller

---
+++ src/main/java/dev/stormgui/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -0,0 +1,47 @@
+package dev.stormgui.webfluxcourse.controller.impl;
+
+import dev.stormgui.webfluxcourse.controller.UserController;
+import dev.stormgui.webfluxcourse.model.request.UserRequest;
+import dev.stormgui.webfluxcourse.model.response.UserResponse;
+import dev.stormgui.webfluxcourse.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserControllerImpl implements UserController {
+
+    private final UserService userService;
+
+    @Override
+    public ResponseEntity<Mono<Void>> save(final UserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.save(request).then());
+    }
+
+    @Override
+    public ResponseEntity<Mono<UserResponse>> find(String id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Flux<UserResponse>> findAll() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Mono<UserResponse>> update(String id, UserRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Mono<Void>> delete(String id) {
+        return null;
+    }
+}
---
+++ src/main/java/dev/stormgui/webfluxcourse/service/UserService.java
@@ -0,0 +1,21 @@
+package dev.stormgui.webfluxcourse.service;
+
+import dev.stormgui.webfluxcourse.entity.User;
+import dev.stormgui.webfluxcourse.mapper.UserMapper;
+import dev.stormgui.webfluxcourse.model.request.UserRequest;
+import dev.stormgui.webfluxcourse.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public Mono<User> save(final UserRequest request) {
+        return userRepository.save(userMapper.toEntity(request));
+    }
+}
