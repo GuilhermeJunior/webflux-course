2264212ab7346fae513163b5fb10e89754d680cd
Guilherme Chamorro <poltegaisnt@gmail.com>
2023-05-24 18:20:00 -0300

    add field validation error

--- src/main/java/dev/stormgui/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/dev/stormgui/webfluxcourse/model/request/UserRequest.java
@@ -1,8 +1,21 @@
 package dev.stormgui.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characteres")
+        @NotBlank(message = "must not be null or empty")
         String name,
+
+        @Email(message = "Invalid email")
+        @NotBlank(message = "must not be null or empty")
         String email,
+
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characteres")
+        @NotBlank(message = "must not be null or empty")
         String password
 ) {
 }
