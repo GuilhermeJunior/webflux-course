56d5d2e982e7f4fa2c5b1cd03c756832e0265660
Guilherme Chamorro <poltegaisnt@gmail.com>
2023-05-24 21:05:27 -0300

    add string validator

--- src/main/java/dev/stormgui/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/dev/stormgui/webfluxcourse/model/request/UserRequest.java
@@ -1,19 +1,23 @@
 package dev.stormgui.webfluxcourse.model.request;
 
+import dev.stormgui.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characteres")
         @NotBlank(message = "must not be null or empty")
         String name,
 
+        @TrimString
         @Email(message = "Invalid email")
         @NotBlank(message = "must not be null or empty")
         String email,
 
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characteres")
         @NotBlank(message = "must not be null or empty")
         String password
